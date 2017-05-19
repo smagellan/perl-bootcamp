59e453ebe307bedcf33408768199e8fd957f2ec3
Vladimir Vishnevsky <smagellan@gmail.com>
2017-05-19 10:47:24 +0300

    added edit page edit logic

--- edit/Edit.pm
+++ edit/Edit.pm
@@ -3,8 +3,11 @@ package edit::Edit;
 use strict;
 use warnings FATAL => 'all';
 
+use DDP;
+use CGI;
 use BootcampDbConn;
 use BootcampCommons qw(extract_id_from_request_string);
+
 
 
 use base 'Exporter';
@@ -12,7 +15,25 @@ our @EXPORT = qw(
     edit_student
     );
 
-sub render_get{
+sub update_student_data {
+    my ($c, $r, $student_id) = @_;
+    my $post_body = $r->content();
+    my $passed_params = CGI->new($post_body)->Vars();
+
+    my $dbh = db_conn();
+    my $rv = $dbh->do("update student set
+                    name = ?, surname = ?, nationality = ?,
+                    address = ?, birthday = ?
+              where id = ?", undef,
+        $passed_params->{"student_name"}, $passed_params->{"student_surname"}, $passed_params->{"student_nationality"},
+        $passed_params->{"student_address"}, $passed_params->{"student_birthday"},
+        $passed_params->{"student_id"});
+    if ($rv < 0) {
+        print $DBI::errstr;
+    }
+}
+
+sub render_get {
     my ($student_id) = @_;
     my $dbh = db_conn();
     my $sth = $dbh->prepare("select st.id, st.name, st.surname, st.sex, st.nationality, st.address, st.birthday, st.mark, gr.id as group_id, gr.nomer
@@ -43,22 +64,27 @@ sub render_get{
 
 sub edit_student {
     my ($c, $r) = @_;
-
     my $r_path = $r->uri->path;
     my $student_id = extract_id_from_request_string($r_path);
     print("student id: $student_id\n");
-    my $answer = "";
-    my $http_code   = 200;
     if ($r->method eq 'GET') {
-        ($http_code, $answer) = render_get($student_id);
+        my ($http_code, $answer) = render_get($student_id);
+        $c->send_basic_header($http_code);
+        print $c "Content-Type: text/html";
+        $c->send_crlf();
+        $c->send_crlf();
+
+        print $c $answer;
+    } else {
+        if ($r->method eq 'POST') {
+            update_student_data($c, $r, $student_id);
+            $c->send_basic_header(302);
+            print $c "Location: /";
+
+            $c->send_crlf();
+            $c->send_crlf();
+        }
     }
-
-    $c->send_basic_header($http_code);
-    print $c "Content-Type: text/html";
-    $c->send_crlf();
-    $c->send_crlf();
-
-    print $c $answer;
 }
 
 
