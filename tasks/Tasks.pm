55ffd84fe8d62f770c9355c821507e4a1e6ef4eb
Vladimir Vishnevsky <smagellan@gmail.com>
2017-05-22 13:26:17 +0300

    tasks progress

--- tasks/Tasks.pm
+++ tasks/Tasks.pm
@@ -12,7 +12,7 @@ our @EXPORT = qw(
 
 sub show_tasks_solutions{
     my ($c, $r) = @_;
-    my $task_result = task1();
+    my $task_result = task5();
 
 
 
@@ -41,9 +41,57 @@ sub show_tasks_solutions{
 sub task1 {
     my $dbh = db_conn();
     my @result = $dbh->selectall_array("select name, surname, mark from student st");
-    print "task1 results:\n";
     my $ret = {
         column_names => ["name", "surname", "mark"],
+        columns      => \@result
+    };
+    return $ret;
+}
+
+sub task2 {
+    my $dbh = db_conn();
+    my @result = $dbh->selectall_array("select st.name, st.surname, st.mark, gr.nomer as group_nomer
+                       from student st inner join group_st gr on (st.group_id=gr.id)
+                       order by surname");
+    my $ret = {
+        column_names => ["name", "surname", "mark", "group_nomer"],
+        columns      => \@result
+    };
+    return $ret;
+}
+
+sub task3 {
+    my $dbh = db_conn();
+    my @result = $dbh->selectall_array("select st.name, st.surname, st.birthday, mark, gr.nomer as group_nomer, fa.name as faculty_name
+                       from student st
+                       inner join group_st gr on (st.group_id = gr.id)
+                       inner join sub_faculty fa on (gr.sub_fac_id = fa.id)
+                       order by surname, birthday");
+    my $ret = {
+        column_names => ["name", "surname", "birthday", "mark", "group_nomer", "faculty_name"],
+        columns      => \@result
+    };
+    return $ret;
+}
+
+sub task4 {
+    my $dbh = db_conn();
+    my @result = $dbh->selectall_array("select st.name, st.surname, gr.nomer as group_nomer
+                       from student st
+                       inner join group_st gr on (st.group_id = gr.id)
+                       where st.nationality not like 'Русск%'");
+    my $ret = {
+        column_names => ["name", "surname","group_nomer"],
+        columns      => \@result
+    };
+    return $ret;
+}
+
+sub task5 {
+    my $dbh = db_conn();
+    my @result = $dbh->selectall_array("select count(1) as student_count from student");
+    my $ret = {
+        column_names => ["student_count"],
         columns      => \@result
     };
     return $ret;
