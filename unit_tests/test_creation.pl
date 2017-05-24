d21c756d7834a64c3c7949ca49c4b625c517e228
Vladimir Vishnevsky <smagellan@gmail.com>
2017-05-24 16:54:36 +0300

    added test logic

--- unit_tests/test_creation.pl
+++ unit_tests/test_creation.pl
@@ -9,7 +9,7 @@ use lib "/home/smagellan/yandex-perl/selenium-web-driver/lib/perl5";
 use lib "/home/smagellan/proj/perl-bootcamp";
 use BootcampSettings;
 use Selenium::Remote::Driver;
-use Test::More tests=>1;
+use Test::More tests=>2;
 
 my $driver = Selenium::Remote::Driver->new({browser_name => "chromium"});
 
@@ -27,4 +27,7 @@ $driver->find_element("student_address", "name")->send_keys("Test Address");
 $driver->find_element("student_mark", "name")->send_keys("5");
 $driver->find_element("submit_form_btn", "name")->click();
 is($driver->get_current_url(), $server_root_url."/", "post-edit redirect to root page");
+my $table_text = $driver->get_text("students_list_table", "id");
+ok($table_text =~ "TestName" && $table_text =~ "TestSurname", "list table contains our's entry");
+
 $driver->quit();
